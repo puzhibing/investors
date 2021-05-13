0014d3b1df16a868aab458f79a50316993c102ac
puzhibing <393733352@qq.com>
2021-05-13 19:12:31 +0800

    更新web页面代码

--- src/main/java/com/puzhibing/investors/controller/TestController.java
+++ src/main/java/com/puzhibing/investors/controller/TestController.java
@@ -5,15 +5,13 @@ import com.puzhibing.investors.service.ISecuritiesMarketService;
 import com.puzhibing.investors.util.ResultUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/test")
 public class TestController {
 
